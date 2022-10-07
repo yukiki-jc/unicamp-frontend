aed337d0ec44bf7969723743ee62816962b05547
cc576935947@outlook.com <cc576935947@outlook.com>
2022-10-07 19:43:52 +0800

    move some files

---
+++ src/pages/MainPage.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { Button } from '@mui/material'
+import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+
+const MainPageCard = props => {
+  const {courseList} = props;
+  return (
+    <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {courseList.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+  )
+}
+
+
+class MainPage extends React.Component {
+  render () {
+    return (
+      <div>
+        Main Page
+        <br/>
+        <Link
+          className={classNames('nav-link')}
+          to={'/login'} 
+        > 
+          <Button variant="contained">
+          Turn to Login Page
+          </Button>
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default MainPage
