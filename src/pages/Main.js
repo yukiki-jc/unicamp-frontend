1e37cf30135d7c4f4d8552bd8bfd88c8294c1219
cc576935947@outlook.com <cc576935947@outlook.com>
2022-10-08 12:23:17 +0800

    ignore package-lock

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,21 +2,13 @@ import React from 'react'
 import { Button } from '@mui/material'
 import { Link } from 'react-router-dom'
 import classNames from 'classnames'
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 
 const MainPageCard = props => {
   const {courseList} = props;
