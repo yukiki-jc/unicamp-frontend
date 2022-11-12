10d6a91c8586b7bf4c3a3efa5178287250a46e61
Shiina <shiinahiiragi@outlook.com>
2022-11-12 19:47:50 +0800

    change X to ✕

--- src/components/RoundAvatar.js
+++ src/components/RoundAvatar.js
@@ -4,7 +4,7 @@ import { getUser } from '../utils/storeUser';
 const RoundAvatar = props => {
   const { sx, displayName, src = null } = props;
   // this chain stands for priviledge of displaying
-  const name = displayName ?? getUser()?.name ?? 'X'
+  const name = displayName ?? getUser()?.name ?? '✕'
   const avatarSrc = src ?? getUser()?.avatar ?? '/'
   return (<Avatar alt={name} sx={sx} src={avatarSrc} />)
 }
